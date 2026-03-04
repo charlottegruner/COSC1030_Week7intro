a76e6febafe7975306ea1fd418ed39e6160e5f6a
Charlotte Gruner <60362002+charlottegruner@users.noreply.github.com>
2026-03-04 12:11:24 -0700

    Add basic array handling in array_intro.cpp
    Implemented a basic C++ program to input and display scores in an array.

---
+++ array_intro.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        const int SIZE = 5;
+        int scores[SIZE];  //size of array needs to be known at compile time 
+                           //
+        // access the memory
+        // indexing notation
+        /*
+        int ind = 0;
+        scores[ind] = 88;
+        ind++;
+        scores[ind] = 79;
+        ind++;
+        cout << "Enter a score to put ind " << ind << ":";
+        cin >> scores[ind];
+        */
+
+        // prompt use for SIZE of entries in a loop and fill the array
+        for (int ind = 0; ind <= SIZE - 1; ind++)
+        {
+                cout << "Enter a score: ";
+                cin >> scores[ind];
+        }
+
+        // print out the array
+        for (int ind = 0; ind <= SIZE - 1; ind++)
+        {
+                cout << scores[ind] << " ";
+        }
+        cout << endl;
+
+        // write a loop to print the  array backwards
+
+        // write a loop to find the smallest element of the array and 
+        // print out the smallest element and which array index it is in
+
+        return 0;
+}
